82fd3bb00cc950b52dca29e5112f789e14be2c05
Raunak Gayen <rnk.express@gmail.com>
2022-08-08 12:30:08 +0530

    added paths in app.js frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
 
+// eslint-disable-next-line
 import { Flex, Heading } from '@chakra-ui/react';
 import { motion, isValidMotionProp } from 'framer-motion';
-import { Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Homepage from './Pages/Homepage';
+import ChatPage from './Pages/ChatPage';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <Route path='/' component={}/>
-      {/* <Route path='/chats' /> */}
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+      <Route path='/chats' element={ <ChatPage /> }/>
+      </Routes>
     </div>
   );
 }
