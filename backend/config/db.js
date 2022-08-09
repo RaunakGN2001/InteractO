af929e5e3583b10e4091a50bc2c68ab869798a38
Raunak Gayen <rnk.express@gmail.com>
2022-08-09 12:52:34 +0530

    Added MongoDB connection to the web server

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const MONGO_URI = "mongodb+srv://interacto_chat:iiest_IT20@cluster0.cka1hed.mongodb.net/?retryWrites=true&w=majority"
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+        process.exit();
+    }
+}
+
+module.exports = connectDB;
